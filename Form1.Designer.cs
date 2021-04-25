61a90a2d103529ab6a0646a1cc348610184f19d2
Max Molyavko <maxmolyt@gmail.com>
2021-04-25 17:21:58 +0300

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace pexeso {
+	partial class Form1 {
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing) {
+			if (disposing && (components != null)) {
+				components.Dispose();
+			}
+
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent() {
+			this.bStart = new System.Windows.Forms.Button();
+			this.LNavod = new System.Windows.Forms.Label();
+			this.LPocetTahu = new System.Windows.Forms.Label();
+			this.bKonec = new System.Windows.Forms.Button();
+			this.button1 = new System.Windows.Forms.Button();
+			this.bHratZnovu = new System.Windows.Forms.Button();
+			this.LVysledek = new System.Windows.Forms.Label();
+			this.SuspendLayout();
+			// 
+			// bStart
+			// 
+			this.bStart.BackColor = System.Drawing.SystemColors.MenuHighlight;
+			this.bStart.Font = new System.Drawing.Font("Segoe UI Black", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte) (0)));
+			this.bStart.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+			this.bStart.Location = new System.Drawing.Point(12, 364);
+			this.bStart.Name = "bStart";
+			this.bStart.Size = new System.Drawing.Size(776, 74);
+			this.bStart.TabIndex = 0;
+			this.bStart.Text = "START";
+			this.bStart.UseVisualStyleBackColor = false;
+			this.bStart.Visible = false;
+			this.bStart.Click += new System.EventHandler(this.bStartClick);
+			// 
+			// LNavod
+			// 
+			this.LNavod.Font = new System.Drawing.Font("Consolas", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte) (0)));
+			this.LNavod.Location = new System.Drawing.Point(12, 9);
+			this.LNavod.Name = "LNavod";
+			this.LNavod.Size = new System.Drawing.Size(776, 352);
+			this.LNavod.TabIndex = 1;
+			this.LNavod.Text = "\r\n\r\n▶ PEXESO ◀\r\n\r\nPro spuštění hry stiskněte \"START\"";
+			this.LNavod.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+			this.LNavod.Visible = false;
+			// 
+			// LPocetTahu
+			// 
+			this.LPocetTahu.Font = new System.Drawing.Font("Consolas", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte) (0)));
+			this.LPocetTahu.Location = new System.Drawing.Point(538, 9);
+			this.LPocetTahu.Name = "LPocetTahu";
+			this.LPocetTahu.Size = new System.Drawing.Size(250, 70);
+			this.LPocetTahu.TabIndex = 2;
+			this.LPocetTahu.Text = "Počet tahů : 0";
+			this.LPocetTahu.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			// 
+			// bKonec
+			// 
+			this.bKonec.BackColor = System.Drawing.Color.LightCoral;
+			this.bKonec.Font = new System.Drawing.Font("Segoe UI Black", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte) (0)));
+			this.bKonec.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+			this.bKonec.Location = new System.Drawing.Point(538, 364);
+			this.bKonec.Name = "bKonec";
+			this.bKonec.Size = new System.Drawing.Size(250, 74);
+			this.bKonec.TabIndex = 3;
+			this.bKonec.Text = "KONEC";
+			this.bKonec.UseVisualStyleBackColor = false;
+			this.bKonec.Click += new System.EventHandler(this.bKonecClick);
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(12, 364);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(75, 23);
+			this.button1.TabIndex = 4;
+			this.button1.Text = "button1";
+			this.button1.UseVisualStyleBackColor = true;
+			// 
+			// bHratZnovu
+			// 
+			this.bHratZnovu.BackColor = System.Drawing.Color.Orange;
+			this.bHratZnovu.Font = new System.Drawing.Font("Segoe UI Black", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte) (0)));
+			this.bHratZnovu.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+			this.bHratZnovu.Location = new System.Drawing.Point(12, 364);
+			this.bHratZnovu.Name = "bHratZnovu";
+			this.bHratZnovu.Size = new System.Drawing.Size(776, 74);
+			this.bHratZnovu.TabIndex = 5;
+			this.bHratZnovu.Text = "Hrát Znovu";
+			this.bHratZnovu.UseVisualStyleBackColor = false;
+			this.bHratZnovu.Visible = false;
+			this.bHratZnovu.Click += new System.EventHandler(this.bHratZnovu_Click);
+			// 
+			// LVysledek
+			// 
+			this.LVysledek.Font = new System.Drawing.Font("Segoe UI Black", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte) (0)));
+			this.LVysledek.Location = new System.Drawing.Point(12, 9);
+			this.LVysledek.Name = "LVysledek";
+			this.LVysledek.Size = new System.Drawing.Size(776, 352);
+			this.LVysledek.TabIndex = 6;
+			this.LVysledek.Text = "Počet tahů\r\n▶ 123 ◀";
+			this.LVysledek.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackColor = System.Drawing.SystemColors.Window;
+			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.Controls.Add(this.LVysledek);
+			this.Controls.Add(this.bHratZnovu);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.bKonec);
+			this.Controls.Add(this.LPocetTahu);
+			this.Controls.Add(this.LNavod);
+			this.Controls.Add(this.bStart);
+			this.Location = new System.Drawing.Point(15, 15);
+			this.Name = "Form1";
+			this.Text = "Pexeso";
+			this.ResumeLayout(false);
+		}
+
+		private System.Windows.Forms.Label LVysledek;
+
+		private System.Windows.Forms.Button bHratZnovu;
+
+		private System.Windows.Forms.Button button1;
+
+		private System.Windows.Forms.Button bKonec;
+
+		private System.Windows.Forms.Label LPocetTahu;
+
+		private System.Windows.Forms.Label LNavod;
+
+		private System.Windows.Forms.Button bStart;
+
+		#endregion
+	}
+}
---
+++ Form1.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace pexeso {
+	public partial class Form1 : Form {
+		// možné stavy hry
+		public enum Page {
+			Start,
+			Hra,
+			Konec
+		};
+		
+		private static int gameSize = 6;
+		
+		// promenna stavu
+		private Page page;
+		private int pocetTahu = 0;
+		private int[] randomizedNumbers;
+		private HashSet<Button> cards = new HashSet<Button>();
+		private Button selectedCard = null;
+		private bool blockCardClick = false;
+
+		private Dictionary<Page, HashSet<Control>> pagesContent = new Dictionary<Page, HashSet<Control>>();
+		
+		public Form1() {
+			Random random = new Random();
+			randomizedNumbers = Enumerable.Range(0, gameSize * gameSize)
+				.OrderBy(x => random.Next()).ToArray();
+
+			InitializeComponent();
+
+			setPageContent(Page.Start, bStart, LNavod);
+			setPageContent(Page.Hra, bKonec, LPocetTahu);
+			setPageContent(Page.Konec, bHratZnovu, LVysledek);
+
+			setPage(Page.Start);
+		}
+		
+		// funkce kde uvedu elementy, které se zobrazí ve stavu `page`
+		private void setPageContent(Page page, params Control[] controls) {
+			pagesContent.Add(page, new HashSet<Control>(controls));
+		}
+
+		// funce nastavení stavu
+		public void setPage(Page page) {
+			var visibleControls = pagesContent[page];
+			foreach (Control control in Controls) {
+				control.Visible = visibleControls.Contains(control);
+			}
+
+			if (page == Page.Hra) {
+				pocetTahu = 0;
+				LPocetTahu.Text = "Počet tahů : 0";
+				createCards();
+			}
+			else
+				removeCards();
+
+			this.page = page;
+		}
+
+		private void bStartClick(object b, EventArgs eventArgs) {
+			setPage(Page.Hra);
+		}
+		
+		private void bHratZnovu_Click(object b, EventArgs eventArgs) {
+			setPage(Page.Hra);
+		}
+		
+		private void bKonecClick(object b, EventArgs eventArgs) {
+			LVysledek.Text = "Konec hry";
+			setPage(Page.Konec);
+		}
+		
+		// funkce vytvaření kartiček
+		private void createCards() {
+			if (cards.Count == 0) {
+				int width = (ClientRectangle.Width - 286) / gameSize;
+				int height = (ClientRectangle.Height - 24) / gameSize;
+
+				for (int i = 0; i < gameSize; i++)
+					for (int j = 0; j < gameSize; j++) {
+						Button b = new Button();
+						b.Width = width;
+						b.Height = height;
+						b.Left = i * width + 12;
+						b.Top = j * height + 12;
+						b.Text = "";
+						b.Tag = randomizedNumbers[gameSize * i + j] / 2 + 1;
+						b.Font = bStart.Font;
+						b.BackColor = Color.Gray;
+						b.Click += cardClickEvent;
+						Controls.Add(b);
+						cards.Add(b);
+					}
+			}
+		}
+		
+		private void removeCards() {
+			foreach (Button button in cards) {
+				Controls.Remove(button);
+				button.Dispose();
+			}
+			
+			cards.Clear();
+		}
+
+		// zkontroluju, zda byly uhádnuty všechny kartičky
+		private bool allCardsFlipped() {
+			foreach (var b in cards) {
+				if (b.Enabled)
+					return false;
+			}
+
+			return true;
+		}
+		
+		private void cardClickEvent(object obj, EventArgs eventArgs) {
+			if (blockCardClick) return;
+
+			Button b = (Button) obj;
+
+			if (selectedCard == b) return;
+			
+			b.Text = b.Tag.ToString();
+			b.BackColor = Color.FloralWhite;
+			
+			if (selectedCard == null) {
+				selectedCard = b;
+			}
+			else {
+				pocetTahu++;
+				LPocetTahu.Text = "Počet tahů : " + pocetTahu;
+				
+				if ((int) selectedCard.Tag == (int) b.Tag) {
+					selectedCard.Click -= cardClickEvent;
+					b.Click -= cardClickEvent;
+					b.Enabled = false;
+					selectedCard.Enabled = false;
+				}
+				else {
+					noMatch(selectedCard, b);
+					blockCardClick = true;
+				}
+
+				selectedCard = null;
+			}
+
+			if (allCardsFlipped()) {
+				LVysledek.Text = "Počet tahů\n▶" + pocetTahu + "◀";
+				setPage(Page.Konec);
+			}
+		}
+
+		private async void noMatch(Button b1, Button b2) {
+			b1.BackColor = Color.LightCoral;
+			b2.BackColor = Color.LightCoral;
+			
+			await Task.Delay(500);
+			b1.Text = "";
+			b2.Text = "";
+			b1.BackColor = Color.Gray;
+			b2.BackColor = Color.Gray;
+			blockCardClick = false;
+		}
+	}
+}
